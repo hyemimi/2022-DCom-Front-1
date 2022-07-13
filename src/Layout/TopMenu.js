1c7ab3fa978f407d18a4c9f78d7bb5bef34cc163
정지원 <wjdwl001@twentyoz.kr>
2022-07-13 21:43:03 +0900

    [Package] lint 설정 완료

--- src/Layout/TopMenu.js
+++ src/Layout/TopMenu.js
@@ -1,84 +1,89 @@
-import { useHistory, Link } from "react-router-dom";
-import React, { useRef, useEffect, useState } from "react";
+import { useHistory, Link } from 'react-router-dom'
+import React, { useRef, useEffect, useState } from 'react'
 import {
   getAuth,
   signInWithEmailAndPassword,
   onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
+  signOut
+} from 'firebase/auth'
 
 const TopMenu = ({ isLoggedIn, setIsLoggedIn }) => {
   useEffect(() => {
     onAuthStateChanged(getAuth(), (user) => {
       if (user) {
-        setIsLoggedIn(true);
+        setIsLoggedIn(true)
       } else {
-        setIsLoggedIn(false);
+        setIsLoggedIn(false)
       }
-    });
-  }, []);
+    })
+  }, [])
 
-  const [isMenuOn, setIsMenuOn] = useState(false);
-  const mobileMenuRef = useRef();
+  const [isMenuOn, setIsMenuOn] = useState(false)
+  const mobileMenuRef = useRef()
 
   const onLogOut = () => {
-    signOut(getAuth());
-  };
+    signOut(getAuth())
+  }
 
   const menuOnToggle = () => {
     if (isMenuOn) {
       mobileMenuRef.current.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest",
-      });
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'nearest'
+      })
     }
-    setIsMenuOn((isMenuOn) => !isMenuOn);
-  };
+    setIsMenuOn((isMenuOn) => !isMenuOn)
+  }
 
-  const history = useHistory();
+  const history = useHistory()
   const onClickBoard = (ref) => {
-    setIsMenuOn(false);
-    history.replace(`/${ref}`);
-  };
+    setIsMenuOn(false)
+    history.replace(`/${ref}`)
+  }
 
   const menuList = [
-      { ref: '',
-        label: '🏠　홈'},
-      { ref: 'search',
-        label:'🔍　검색하기'}
-  ];
+    {
+      ref: '',
+      label: '🏠　홈'
+    },
+    {
+      ref: 'search',
+      label: '🔍　검색하기'
+    }
+  ]
 
   return (
     <div className="TopMenu">
       <div className="NavBar">
         <Link to="/">
-          <div className="logo" style={{ color: "#ffc83d" }}>
+          <div className="logo" style={{ color: '#ffc83d' }}>
             DCOM
           </div>
         </Link>
-        {!isLoggedIn ? (
+        {!isLoggedIn
+          ? (
           <Link to="login">
             <button className="loginBtn">로그인</button>
           </Link>
-        ) : (
+            )
+          : (
           <div className="loginBtn" onClick={onLogOut}>
             {getAuth().currentUser.email}
           </div>
-        )}
+            )}
         <button className="hamburgerBtn" onClick={menuOnToggle}>
           ≡
         </button>
       </div>
 
-
-      <div className={isMenuOn ? "isMenuOn" : "isMenuOff"} ref={mobileMenuRef}>
+      <div className={isMenuOn ? 'isMenuOn' : 'isMenuOff'} ref={mobileMenuRef}>
         {/* 모바일 메뉴 리스트 */}
-        {menuList.map(({ref, label})=>(
+        {menuList.map(({ ref, label }) => (
             <button
                 key={ref}
                 onClick={() => {
-                  onClickBoard(ref);
+                  onClickBoard(ref)
                 }}
             >
               {label}
@@ -88,7 +93,7 @@ const TopMenu = ({ isLoggedIn, setIsLoggedIn }) => {
         <div className="mobileCopyright"></div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TopMenu;
+export default TopMenu
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import './index.css'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-import { render } from "react-dom";
+import { render } from 'react-dom'
 
 render(
     <BrowserRouter>
         <App />
     </BrowserRouter>,
-    document.getElementById("root")
-);
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
