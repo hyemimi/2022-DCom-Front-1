97a1744fcfb25718727d5635693a35165e8cdfe3
hyemimi <hyemi7375@gmail.com>
2022-07-13 19:57:02 +0900

    first Groups
    # Conflicts:
    #	src/App.js
    #	src/Layout/SideMenu.js
    #	src/pages/Groups.js
    #	yarn.lock

--- src/App.js
+++ src/App.js
@@ -14,15 +14,43 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  onAuthStateChanged
-} from 'firebase/auth'
+  onAuthStateChanged,
+} from "firebase/auth";
+import { BrowserRouter } from "react-router-dom";
+import Groups from "./pages/Groups";
 
-function App () {
-  useEffect(() => {
-  }, [])
+function App() {
+  useEffect(() => {}, []);
+  const dummyInfo = [
+    { studyId: [0, 2], name: "이혜미", email: "gkj8963@khu.ac.kr" },
+    { studyid: [1], name: "정혜인", email: "hyemi@khu.ac.kr" },
+  ];
 
-  const [data, setData] = useState([])
-  const [activeMenu, setActiveMenu] = useState('home')
+  const dummyList = [
+    {
+      studyId: 0,
+      name: "영어스터디",
+      leader: "gkj8963@khu.ac.kr",
+      member: ["a8963@khu.ac.kr", "kkk@naver.com"],
+    },
+    {
+      studyId: 1,
+      name: "수학스터디",
+      leader: "nav@naver.com",
+      member: ["gkj8963@khu.ac.kr"],
+    },
+    {
+      studyId: 2,
+      name: "파이썬스터디",
+      leader: "sssd@khu.ac.kr",
+      member: ["gkj8963@naver.com"],
+    },
+    { studyId: 3, name: "스피킹스터디", leader: "a", member: [""] },
+  ];
+  const [data, setData] = useState([]);
+  const [info, setInfo] = useState(dummyInfo); // 전체 개인 data
+  const [studyList, setStudyList] = useState(dummyList); // 전체 스터디 리스트
+  const [activeMenu, setActiveMenu] = useState("home");
 
   // firebase
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -56,16 +84,33 @@ function App () {
             </>
           )}
         />
-          <Route exact path="/record" render={() => (
-              <>
-                  <TopMenu isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-                  <Record/>
-              </>)} />
-          <Route exact path="/friends" render={() => (
-          <>
+        <Route
+          exact
+          path="/record"
+          render={() => (
+            <>
               <TopMenu isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-              <Friends/>
-          </>)} />
+              <Record />
+            </>
+          )}
+        />
+        <Route
+          exact
+          path="/groups"
+          render={() => (
+            <>
+              <TopMenu isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+              <Groups
+                activeMenu={activeMenu}
+                setActiveMenu={setActiveMenu}
+                info={info}
+                setInfo={setInfo}
+                studyList={studyList}
+                setStudyList={setStudyList}
+              />
+            </>
+          )}
+        />
       </BrowserRouter>
     </div>
   )
