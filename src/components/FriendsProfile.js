b677d3de2c435a6f43cee3b53af6d4daaf10fe02
Hyein Jeong <jhi2359@naver.com>
2022-07-20 00:22:14 +0900

    modify

--- src/components/FriendsProfile.js
+++ src/components/FriendsProfile.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
-import SideMenu from '../Layout/SideMenu'
 
 function FriendsProfile ({ image, name, nickname, email }) {
   return (
