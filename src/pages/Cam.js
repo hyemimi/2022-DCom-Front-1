a4eae5ffc996f97a41cacbf9f0c64871f144ccdd
JeongHyein <jhi2359@naver.com>
2022-07-22 17:56:14 +0900

    webcam연결

---
+++ src/pages/Cam.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useRef } from 'react'
+
+import Webcam from "react-webcam";
+
+
+const WebcamComponent = () => {
+    return(
+        <div className='webcam'>
+            <Webcam />
+        </div>)
+}
+
+
+export default WebcamComponent
