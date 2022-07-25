de35ebd1208a9897a9f448fee85c5c2ddd6acef8
hyemimi <hyemi7375@gmail.com>
2022-07-25 22:34:52 +0900

    [Groups.js]props제거/redux dummy데이터 사용

--- src/components/GroupItem.js
+++ src/components/GroupItem.js
@@ -1,24 +1,25 @@
 // GroupItem은 그룹별 프로필과 같다고 보면 된다. 그룹 리스트를 나열할 때 컴포넌트로 사용될 예정
-import React, { useEffect, useState, useRef } from 'react'
-import GroupButton from './GroupButton'
-function GroupItem({name,leader,members, buttontext}) {
-
-    return(
-             <div >
-                <div className="group-name">{name}</div>
-                <div>그룹장 | {leader}</div>
-                {members && members.map((item, idx) => {
-                  return (
-                    // eslint-disable-next-line react/jsx-key
-                    <div>
-                      {item === '' ? 'Empty' : `member${idx + 1} | ${item}`}
-                    </div>
-                  )
+import React, { useEffect, useState, useRef } from 'react';
+import GroupButton from './GroupButton';
+function GroupItem({ name, leader, members, buttontext }) {
+    return (
+        <div>
+            <div className="group-name">{name}</div>
+            <div>그룹장 | {leader}</div>
+            {members &&
+                members.map((item, idx) => {
+                    return (
+                        // eslint-disable-next-line react/jsx-key
+                        <div>
+                            {item === ''
+                                ? 'Empty'
+                                : `member${idx + 1} | ${item.nickname}`}
+                        </div>
+                    );
                 })}
-                <GroupButton text={buttontext}></GroupButton>
+            <GroupButton text={buttontext}></GroupButton>
         </div>
-
-  )
+    );
 }
 
-export default GroupItem
+export default GroupItem;
