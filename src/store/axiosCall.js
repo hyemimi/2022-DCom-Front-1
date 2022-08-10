fe0d74a2b69faaf1674edc40f714b3b6e9f20cdc
wjdwl002 <wjdwl001@khu.ac.kr>
2022-08-10 20:16:47 +0900

    [공통] axios 함수 수정

--- src/store/axiosCall.js
+++ src/store/axiosCall.js
@@ -1,23 +1,32 @@
 import axios from 'axios';
 
 const getAxios = (endPoint, data={}) => axios({
-    url: `http://focuz.justkode.kr:8080/${endPoint}`,
+    url: `http://focuz.justkode.kr:8080${endPoint}`,
     method: 'get',
     data: data,
 });
 
 const postAxios = (endPoint, params={})=>axios({
-    url: `http://focuz.justkode.kr:8080/${endPoint}`,
+    url: `http://focuz.justkode.kr:8080${endPoint}`,
     method: 'POST',
     params: params,
 });
 
 const deleteAxios = (endPoint)=>axios({
-    url: `http://focuz.justkode.kr:8080/${endPoint}`,
+    url: `http://focuz.justkode.kr:8080${endPoint}`,
     method: 'DELETE',
 })
 
-getAxios({endPoint: '/user/list'}).
+
+// 사용예시
+getAxios('/user/list').
     then((res)=>{
         console.log(res);
     })
+
+postAxios('/user/register', {
+    "motto": "string",
+    "nickname": "string"
+}).then((res)=>{
+    console.log(res);
+})
