bca9d2b61bdc036bb8fcd8aa609458944953fb1e
wjdwl002 <wjdwl001@khu.ac.kr>
2022-08-13 19:08:58 +0900

    eslint issue fix

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Test = () => {
-    return <div> 테스트용 페이지입니다.  </div>
-}
+    return <div> 테스트용 페이지입니다.  </div>;
+};
 
-export default Test
+export default Test;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import SideMenu from '../Layout/SideMenu.js'
-import { Link } from 'react-router-dom'
-import MainPage from '../components/Main/MainPage.js'
-import InitialPage from '../components/Main/InitialPage'
+import SideMenu from '../Layout/SideMenu.js';
+import { Link } from 'react-router-dom';
+import MainPage from '../components/Main/MainPage.js';
+import InitialPage from '../components/Main/InitialPage';
 
-const Main = ({  isLoggedIn, setIsLoggedIn,userObj ,users}) => {
-
-  return (
+const Main = ({ isLoggedIn, setIsLoggedIn, userObj, users }) => {
+    return (
     <>
-      <div className={isLoggedIn?'content':''}>
+      <div className={isLoggedIn ? 'content' : ''}>
         <div className="MainPage">
           <div>
             {!isLoggedIn
@@ -19,13 +18,13 @@ const Main = ({  isLoggedIn, setIsLoggedIn,userObj ,users}) => {
         </div>
       </div>
     </>
-  )
-}
+    );
+};
 
 Main.propsTypes = {
-  isLoggedIn: PropTypes.bool,
-  setIsLoggedIn: PropTypes.func,
-  userObj: PropTypes.bool,
-}
+    isLoggedIn: PropTypes.bool,
+    setIsLoggedIn: PropTypes.func,
+    userObj: PropTypes.bool
+};
 
-export default Main
+export default Main;
