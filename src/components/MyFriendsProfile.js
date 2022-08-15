60b82f92fe02c6e506a6e803daf6360ceaf04644
JeongHyein <jhi2359@naver.com>
2022-08-15 13:45:44 +0900

    friends페이지 프로필 완성, 더미리스트 등록

--- src/components/MyFriendsProfile.js
+++ src/components/MyFriendsProfile.js
@@ -3,9 +3,10 @@ import styled, { useTheme } from 'styled-components';
 import { useAuth } from '../Context/auth';
 import { blcokRequest, deleteFriend } from './../store/friend';
 import { Box } from '../components/Styled/Box';
+import { faAlignCenter } from '@fortawesome/fontawesome-free-solid';
 
 function MyFriendsProfile ({user}) {
-    const { profileImage, nickname, id } = user;
+    const { profileImage, name, motto, nickname, id } = user;
     const [isRequest, setIsRequest] = useState(false);
     const theme = useTheme();
     const auth = useAuth();
@@ -22,13 +23,15 @@ function MyFriendsProfile ({user}) {
         }
     };
     return (
-        <Box theme={theme} width="200px" height="240px">
-            <div style={{height: '130px', maxHeight: '130px'}}>
+        <Box theme={theme} width="200px" height="300px">
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', height: '130px', maxHeight: '130px', vericalAlgin: 'center', }}>
                 {profileImage && (
-                    <img src={profileImage} width="100px" height="100px" />
+                    <img src={profileImage} width="150px" height="100px" />
                 )}
             </div>
-            <p>{nickname}</p>
+            <div style={{fontSize: '20px', fontWeight: 'bold', paddingTop: '15px'}}>{nickname}</div>
+            {/*<div style={{fontSize: '15px'}}>{name}</div>*/}
+            <div style={{fontSize: '10px', paddingBottom: '15px'}}>{motto}</div>
             <button 
                 className={auth.user?.id === user.id ? 'disabled' : ''}
                 disabled={auth.user?.id === user.id ? true: false}
--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -9,7 +9,32 @@ import { PageDiv } from './SearchFriend';
 import { SearchBox } from '../components/Common/SearchBox';
 
 const Friends = () => {
-    const auth = useContext(AuthContext);
+    const friendummylist = [
+        // dummy
+        // 이후 allFriendsList로 바꾸기
+        {
+            id: 1,
+            motto: '파이팅!!!',
+            name: '정혜인',
+            nickname: '혠',
+            profileImage: 'https://images.mypetlife.co.kr/content/uploads/2021/10/19151330/corgi-g1a1774f95_1280-1024x682.jpg',
+        },
+        {
+            id: 2,
+            motto: '열공중!',
+            name: '정지원',
+            nickname: '원',
+            profileImage: 'http://www.chemicalnews.co.kr/news/photo/202106/3636_10174_4958.jpg',
+        },
+        {
+            id: 3,
+            motto: '열심히!',
+            name: '이혜미',
+            nickname: '혬',
+            profileImage: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        }
+    ];
+
     const [allFriendsList, setAllFriendsList] = useState();
 
     useEffect(()=>{
@@ -21,26 +46,34 @@ const Friends = () => {
 
     return (
         <PageDiv>
-            <h1 className="friendsheader"> My Friends </h1>
+            <h1 className="friendsheader">
+                <a
+                    style={{
+                        color: '#ffc83d',
+                        fontSize: 'min(6vw, 40px)',
+                    }}
+                >
+                    {' '}My Friends{' '}
+                </a>
+            </h1>
             <div style={{ display: 'flex' }}>
                 <Link key="SearchFriend" to="/search-friend">
-                    <Button className="friendsAddBtn">
+                    <Button>
                         🔍　친구 추가 하러가기
                     </Button>
                 </Link>
 
                 <Link key="requestFriends" to="/request-friends">
-                    <Button className="friendsAddBtn">
+                    <Button>
                         📩　친구 요청 확인하기
                     </Button>
                 </Link>
             </div>
-
-
+            📑{friendummylist.length}명의 친구가 있습니다
             {/*<SearchBox onChange={onChange} placeholder="친구의 닉네임을 입력하세요"/>*/}
-            <div className="FriendsProfileBoxes" style={{display: 'flex', flexWrap: 'wrap'}}>
-                {allFriendsList
-                    ? allFriendsList.map((it) => {
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                {friendummylist
+                    ? friendummylist.map((user) => {
                             return (
                                 <>
                                 <MyFriendsProfile key={user.id}
