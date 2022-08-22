efef3f91b19f579814521754fe1f4d7787fba19b
wjdwl002 <wjdwl001@khu.ac.kr>
2022-08-22 21:53:57 +0900

    [그룹생성하기] 그룹생성하기 페이지 구현

--- src/pages/NewGroup.js
+++ src/pages/NewGroup.js
@@ -7,27 +7,34 @@ import { BasicInput, TextareaInput } from '../components/Styled/Input';
 const NewGroup = () => {
     const [inputs, setInputs] = useState({
         name: '',
-        description: '',
+        description: ''
     });
+
     const onChange = (e) => {
         if (e.target.name === 'name') {
-            setInputs({
-                name: e.target.value,
-                description: inputs.description,
-            });
+          setInputs({
+              name: e.target.value,
+              description: inputs.description
+          });
         }
         if (e.target.name === 'description') {
-            setInputs({ name: inputs.name, description: e.target.value });
+          setInputs({ name: inputs.name, description: e.target.value });
         }
+        console.log(inputs);
     };
-    const onSubmit = async (e) => {
-        if (inputs.name.length >= 4 && inputs.description.length >= 4) {
-            await createGroup({
-                description: inputs.description,
-                name: inputs.name,
-            });
+
+    const onSubmit = (e) => {
+        if ((inputs.name.length >= 4) && (inputs.description.length >= 4)) {
+          e.preventDefault();
+          createGroup({
+              description: inputs.description,
+              name: inputs.name
+          }).then(() => {
+              alert('그룹 생성이 완료되었습니다!');
+          })
+          ;
         } else {
-            alert('네 글자 이상의 그룹명과 설명을 입력해주세요.');
+          alert('네 글자 이상의 그룹명과 설명을 입력해주세요.');
         }
     };
 
@@ -39,54 +46,36 @@ const NewGroup = () => {
                     justifyContent: 'center',
                     gap: '0.5em',
                     flexDirection: 'column',
-                    alignItems: 'center',
+                    alignItems: 'center'
                 }}
             >
                 <h1 style={{ color: '#ffc83d' }}> 스터디그룹 생성하기 </h1>
                 <Box width="80%" style={{ maxWidth: '800px' }}>
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            gap: '0.5em',
-                            width: '100%',
-                        }}
-                    >
-                        <BasicInput
-                            placeholder="그룹명"
-                            name="name"
-                            type="text"
-                            width="100%"
-                            value={inputs.name}
-                            onChange={onChange}
-                        />
-                        <TextareaInput
-                            placeholder="설명"
-                            name="description"
-                            width="100%"
-                            height="240px"
-                            value={inputs.description}
-                            onChange={onChange}
-                        />
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5em', width: '100%' }}>
+                    <BasicInput
+                        placeholder="그룹명"
+                        name="name"
+                        type="text"
+                        width="100%"
+                        value={inputs.name}
+                        onChange={(e) => { onChange(e); }}
+                    />
+                    <TextareaInput
+                        placeholder="설명"
+                        name="description"
+                        width="100%"
+                        height="240px"
+                        value={inputs.description}
+                        onChange={(e) => { onChange(e); }}
+                    />
 
-                        <div
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                gap: '0.5em',
-                            }}
-                        >
-                            <Link to="/">
-                                <button>취소하기</button>
-                            </Link>
-                            <button
-                                className="light"
-                                onClick={() => onSubmit()}
-                            >
-                                추가하기
-                            </button>
-                        </div>
+                    <div style={{ display: 'flex', justifyContent: 'center', gap: '0.5em' }}>
+                        <Link to="/">
+                            <button>취소하기</button>
+                        </Link>
+                        <button className='light' onClick={(e) => onSubmit(e)}>추가하기</button>
                     </div>
+                </div>
                 </Box>
             </div>
         </>
