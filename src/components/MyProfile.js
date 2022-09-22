0b9fd78c1109ba035a6e044fba808c3f73ece372
hyemimi <hyemi7375@gmail.com>
2022-09-22 20:00:35 +0900

    [마이페이지]프로필, 페이지 이동 버튼

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,32 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useAuth } from '../Context/auth';
+import { PageDiv } from './Styled/PageDiv';
+import { Box } from './Styled/Box';
+import { Link } from 'react-router-dom';
 
 const MyInfoDiv = styled.div`
     text-align: center;
     font-weight: bold;
-    >*:nth-child(1){
+    > *:nth-child(1) {
         font-size: 24px;
     }
-    >*:nth-child(1){
+    > *:nth-child(1) {
         font-size: 18px;
     }
-    >*:nth-child(1){
+    > *:nth-child(1) {
         font-size: 24px;
     }
-`
+`;
+const Button = styled.button`
+    width: ${(props) => (props.btn ? '230px' : '450px')};
+    height: 100px;
+    padding: 10px 20px;
+    background-color: ${(props) => props.color};
+    border-radius: 0px;
+
+    font-size: 30px;
+`;
 
 function MyProfile() {
     const auth = useAuth();
-    const {user} = auth;
+    const { user } = auth;
     const { profileImage, name, nickname, motto } = user;
 
     return (
-        <div className="Profile">
-            <p className='line'></p>
-            <h1 style={{fontSize: '24px', textAlign: 'center', fontWeight: 'bold'}}>MY PROFILE</h1>
+        <PageDiv>
+            <h1 className="friendsheader">
+                <a
+                    style={{
+                        color: '#ffc83d',
+                        fontSize: 'min(6vw, 40px)',
+                    }}
+                >
+                    My Page
+                </a>
+            </h1>
             <div>
-                {profileImage ? (
+                <Box>
+                    {profileImage ? (
                         <img src={profileImage} width="300px" height="300px" />
                     ) : (
                         <img
@@ -34,15 +55,23 @@ function MyProfile() {
                             width="300px"
                             height="300px"
                         />
-                    )}  
-                <MyInfoDiv>
-                    <div>{name} ({nickname})</div>
+                    )}
+
+                    <div>
+                        {name} ({nickname})
+                    </div>
                     <p>{motto && `motto : ${motto}`}</p>
-                </MyInfoDiv>
+                </Box>
             </div>
-            
-            
-        </div>
+            <div>
+                <Link key="groups" to="/groups">
+                    <Button btn>그룹 관리하기</Button>
+                </Link>
+                <Link key="friends" to="/friends">
+                    <Button btn>친구 관리하기</Button>
+                </Link>
+            </div>
+        </PageDiv>
     );
 }
 
--- src/store/axiosCall.js
+++ src/store/axiosCall.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
 export const getAxios = (endPoint, data = {}, config = {}) =>
-  axios({
-      url: `http://focuz-api.justkode.kr${endPoint}`,
-      method: 'get',
-      data: data,
-      ...config,
-  });
+    axios({
+        url: `http://localhost:8080${endPoint}`,
+        method: 'get',
+        data: data,
+        ...config,
+    });
 
-export const postAxios = (endPoint, params = {}, config={}) =>
-  axios({
-      url: `http://focuz-api.justkode.kr${endPoint}`,
-      method: 'POST',
-      data: params,
-      ...config
-  });
+export const postAxios = (endPoint, params = {}, config = {}) =>
+    axios({
+        url: `http://localhost:8080${endPoint}`,
+        method: 'POST',
+        data: params,
+        ...config,
+    });
 
 export const deleteAxios = (endPoint) =>
-  axios({
-      url: `http://focuz-api.justkode.kr${endPoint}`,
-      method: 'DELETE'
-  });
+    axios({
+        url: `http://localhost:8080${endPoint}`,
+        method: 'DELETE',
+    });
 
 // // 사용예시
 // getAxios('/user/list').then((res) => {
