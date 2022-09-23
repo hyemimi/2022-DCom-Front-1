8bb799c650e7eb0b4299d45c756bf313b23f23bb
hyemimi <hyemi7375@gmail.com>
2022-09-23 19:09:52 +0900

    axios post

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,20 +1,35 @@
-import React, { useRef } from 'react';
-
+import React, { useRef, useState, useEffect } from 'react';
+import { getCurrentUserInfo } from '../store/user';
+import { registerUser } from '../store/user';
+import axios from 'axios';
 const SignUp = () => {
-  
+    const [member, setismember] = useState(false);
+    const [currentUserMotto, setCurrentUserMotto] = useState(null);
+    useEffect(() => {
+        axios
+            .post('http://localhost:8080/user/register', {
+                motto: '.....',
+                nickname: '......',
+            })
+            .then(function (response) {
+                getCurrentUserInfo().then((r) => console.log(r.data));
+            })
+            .catch(function (error) {
+                // 오류발생시 실행
+            });
+    }, []); // 로그인 처음 했을 때
     return (
-    <>
-      <div className="box">
-        <div className="title">DCOM STUDY 회원가입</div>
-        <div className="subTitle">
-          DCOM STUDY 계정으로 모든 서비스 이용이 가능합니다.
-        </div>
+        <>
+            {}{' '}
+            <div className="box">
+                <div className="title">DCOM STUDY 회원가입</div>
+                <div className="subTitle">
+                    DCOM STUDY 계정으로 모든 서비스 이용이 가능합니다.
+                </div>
 
-        <button className="signUpBtn">
-          회원가입
-        </button>
-      </div>
-    </>
+                <button className="signUpBtn">회원가입</button>
+            </div>
+        </>
     );
 };
 
